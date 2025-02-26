eac198028731dcfc3ab65230890783bc076abfc4
Janhavi Desale <desalejanhvi@gmail.com>
2025-02-26 11:32:24 +0530

    Index add

---
+++ script.js
@@ -0,0 +1,6 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+  
+    const nameElement = document.getElementById('name');
+    // nameElement.textContent = 'Janhvi Bharatkumar Desale'; 
+});
